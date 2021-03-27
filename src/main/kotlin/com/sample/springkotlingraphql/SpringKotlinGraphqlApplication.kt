9a6c75527c86f3499619798bdedfb6d8a915d0e1
Shoan Chikato <sjchikato@gmail.com>
2021-03-27 09:24:12 +0200

    init commit
    - example code from presentation
      title: GOTO 2020 • Bootiful GraphQL with Kotlin • Dariusz Kuc
      link: https://www.youtube.com/watch?v=1siPT1pTXFU

---
+++ src/main/kotlin/com/sample/springkotlingraphql/SpringKotlinGraphqlApplication.kt
@@ -0,0 +1,11 @@
+package com.sample.springkotlingraphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinGraphqlApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringKotlinGraphqlApplication>(*args)
+}
